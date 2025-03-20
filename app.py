4caa6d7899fc5e5d648d805cd2e2d6890047bae5
reubenforman <reubenforman1@gmail.com>
2025-03-20 15:21:12 +0000

    Updating plot size

--- app.py
+++ app.py
@@ -476,13 +476,13 @@ if predict_button:
                 show=False
             )
             # Explicitly set the figure size
-            fig.set_size_inches(8, 6)
+            fig.set_size_inches(12, 6)
 
             # Turn off the grid for all axes in the figure
             for ax in fig.axes:
                 ax.grid(False) 
             # Improve plot appearance
-            plt.title('Top Factors Affecting Cancellation Probability', fontsize=8)
+            plt.title('Top Factors Affecting Cancellation Probability', fontsize=10)
             plt.tight_layout()
             
             st.pyplot(fig)
